d6ec34b3ae72d0820e88195b69b4a2e16a190b6f
Asane <yaradayo317@gmail.com>
2021-07-17 07:31:36 -0400

    implementing router and product detail screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,25 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Container } from "react-bootstrap";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
 import HomeScreen from "./screen/HomeScreen";
+import ProductScreen from "./screen/ProductScreen";
 
 const App = () => {
   return (
-    <div>
+    <Router>
       <Header />
       <main className="py-3">
         <Container>
-          <HomeScreen />
+          <Route path="/" exact component={HomeScreen} />
+          <Route path="/product/:id" component={ProductScreen} />
         </Container>
       </main>
 
       <Footer />
-    </div>
+    </Router>
   );
 };
 
